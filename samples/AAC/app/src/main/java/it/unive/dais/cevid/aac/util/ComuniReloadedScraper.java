4d19651b94c3b93b354821e64855e306b295940e
Unknown <851884@stud.unive.it>
2017-11-08 16:49:12 +0100

    aggiunta opzione di estrarre più di una pagina

--- samples/AAC/app/src/main/java/it/unive/dais/cevid/aac/util/ComuniReloadedScraper.java
+++ samples/AAC/app/src/main/java/it/unive/dais/cevid/aac/util/ComuniReloadedScraper.java
@@ -14,37 +14,39 @@ import java.util.List;
  * dal quale si estraggono i dati dei comuni.
  *
  * TODO: renderlo un AsyncTask
- * TODO: generalizzare il numero di pagine da scaricare
  * Created by fbusolin on 08/11/17.
  */
 
-public class ComuniReloadedScraper {
+public class ComuniReloadedScraper{
+    private static final String TAG = "ComuniReloadedScraper";
+
     final String base = "http://soldipubblici.thefool.it/";
 
-    List<Data> scrape(String query){
-        String key = "hidden-phone";
+    List<Data> scrape(String field, int pages){
         List<Data> result = new ArrayList<>();
-        Document doc = null;
-        try {
-            doc = Jsoup.connect(base + query).get();
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        if(doc != null){
-            Elements rows = doc.select("tr");
-            Elements rawElements = rows.select("td");
-            List<String> content = rawElements.eachText();
+        for(int page = 0; page < pages; page++){
+            Document doc = null;
+            try {
+                doc = Jsoup.connect(base + field+"/"+page).get();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            if(doc != null){
+                Elements rows = doc.select("tr");
+                Elements rawElements = rows.select("td");
+                List<String> content = rawElements.eachText();
 
-            for(int i = 0; i < content.size();i+=4){
-                Data data = new Data();
+                for(int i = 0; i < content.size();i+=4){
+                    Data data = new Data();
 
-                data.nome = content.get(i);
-                data.tag = content.get(i+1);
-                data.spesaTotale = content.get(i+2);
-                data.spesaProcapite = content.get(i+3);
+                    data.nome = content.get(i);
+                    data.tag = content.get(i+1);
+                    data.spesaTotale = content.get(i+2);
+                    data.spesaProcapite = content.get(i+3);
 
-                result.add(data);
+                    result.add(data);
+                }
             }
         }
         return result;
@@ -56,5 +58,11 @@ public class ComuniReloadedScraper {
         String tag;
         String spesaTotale;
         String spesaProcapite;
+
+
+        @Override
+        public String toString(){
+            return String.format("{%s,%s,%s,%s}", nome,tag,spesaTotale,spesaProcapite);
+        }
     }
 }
