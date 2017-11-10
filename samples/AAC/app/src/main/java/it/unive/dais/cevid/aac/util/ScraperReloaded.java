e9ec190aefa94a175817fb30563de714fda63ae4
Unknown <851884@stud.unive.it>
2017-11-10 11:58:46 +0100

    Finito Scraper Reloaded
    Ora è un AsyncTask e permette di navigare un numero arbitrario di pagine

--- samples/AAC/app/src/main/java/it/unive/dais/cevid/aac/util/ScraperReloaded.java
+++ samples/AAC/app/src/main/java/it/unive/dais/cevid/aac/util/ScraperReloaded.java
@@ -1,5 +1,8 @@
 package it.unive.dais.cevid.aac.util;
+import android.support.annotation.NonNull;
+
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
@@ -10,41 +13,79 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-/**
- * Created by fbusolin on 09/11/17.
- */
+import it.unive.dais.cevid.datadroid.lib.parser.AbstractAsyncParser;
+import it.unive.dais.cevid.datadroid.lib.util.ProgressStepper;
 
-public class ScraperReloaded {
+/**
+ * Sottoclasse di {@code AbstractAsyncParser} che implementa un downloader e parser per il sito http://soldipubblici.thefool.it/
+ * Questa classe &egrave; usabile direttamente e non necessita di essere ereditata.
+ * Non richiede il generic FiltrableData perch&eacute; utilizza una classe innestata apposita per rappresentare i dati.
+ * La classe {@link ScraperReloaded.Data} ha a sua volta due sottoclassi {@link ScraperReloaded.Comune} e
+ * {@link ScraperReloaded.Siope} che rappresentano i due possibili dati trovabili nel sito.
+ * In fase di scraping viene individuato il tipo corretto tra i due in base al contenuto della pagina.
+ * Un esempio d'uso
+ * <blockquote><pre>
+ * {@code
+ * {@link ScraperReloaded scraper = new ScraperReloaded("comuni",5);
+ * List<{@link ScraperReloaded.Data}> data = scraper.parse();
+ * for(Data d : data){
+ *     //do something
+ * }
+ * }
+ * List<SoldipubbliciParser.FiltrableData> rows = parser.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR).get();
+ * for (CsvRowParser.Row row : rows) {
+ *     String id = row.get("ID"), nome = row.get("NAME");
+ *     // fai qualcosa con id e nome
+ * }
+ * }
+ * </pre></blockquote>
+ * @author fbusolin
+ *
+ *     */
+
+public class ScraperReloaded extends AbstractAsyncParser<ScraperReloaded.Data, ProgressStepper> {
+    private static final String TAG = "ScraperReloaded";
+    String home = "http://soldipubblici.thefool.it/";
     String url;
-    public ScraperReloaded(String url){
-        this.url = url;
+    int depth;
+    public ScraperReloaded(String query,int depth){
+        this.url = this.home + query;
+        this.depth = depth;
     }
+
     public List<Data> scrape(){
+        ProgressStepper progress = new ProgressStepper(depth);
         List<Data> data = new ArrayList<>();
-        Connection connection = Jsoup.connect(url);
-        Document document = null;
-        try{
-            document = connection.get();
-        }catch(IOException ex){
-            Logger.getLogger(ScraperReloaded.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        if(document == null)return data;
-        Elements contentTable = document.select(".table-hover");
-        Elements rows = contentTable.select("tr");
-        for(Element elm : rows.subList(1,rows.size())){
-            String texts = "";
-            Elements tokens = elm.select("td");
-            for(Element piece: tokens){
-                texts +=piece.text() + ";";
+        for(int level = 0; level < this.depth;level++){
+            Connection connection = Jsoup.connect(url + "/" + level);
+            Document document = null;
+            try{
+                document = connection.get();
+            }catch(IOException ex){
+                Logger.getLogger(ScraperReloaded.class.getName()).log(Level.SEVERE, null, ex);
             }
-            switch(texts.charAt(0)){
-                case 'C':
-                    data.add(produceComuni(texts));
-                    break;
-                case 'S':
-                    data.add(produceSiopi(texts));
-                    break;
+            if(document == null)return data;
+            Elements contentTable = document.select(".table-hover");
+            Elements rows = contentTable.select("tr");
+            ProgressStepper parseStepper = progress.getSubProgressStepper(rows.size() - 1);
+            for(Element elm : rows.subList(1,rows.size())){
+                String texts = "";
+                Elements tokens = elm.select("td");
+                for(Element piece: tokens){
+                    texts +=piece.text() + ";";
+                }
+                switch(texts.charAt(0)){
+                    case 'C':
+                        data.add(produceComuni(texts));
+                        break;
+                    case 'S':
+                        data.add(produceSiopi(texts));
+                        break;
+                }
+                parseStepper.step();
             }
+            progress.step();
+            publishProgress(progress);
         }
         return data;
 
@@ -70,7 +111,14 @@ public class ScraperReloaded {
         return result;
     }
 
-    public class Data{String codice;}
+    @NonNull
+    @Override
+    public List<Data> parse() throws IOException {
+        return scrape();
+    }
+
+    public class Data implements Serializable{String codice;}
+
     public class Comune extends Data{
         String nome,
                 spesaTotale,
