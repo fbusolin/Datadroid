b9b78a2381c35a5011091c95cfe4edb3ad2b4c4c
Gianmarco Callegher <854612@stud.unive.it>
2017-11-14 23:35:27 +0100

    Added SearchInterface. Added ListView that represents the various territorial entities and each of them is clickable. When users click over one entitie a new Intent is created and the SearchableActivity starts. The Extra passed are: descrizione_ente, numero_abitanti and all the expenses of the entitie obtained by calling SoldiPubbliciParser that require codice_comparto and codice_ente of the entitie, obtained by calling a listener on the click over the view that rapresent the choice

--- samples/SPRApp/app/src/main/java/com/example/gianmarcocallegher/sprapp/SearchActivity.java
+++ samples/SPRApp/app/src/main/java/com/example/gianmarcocallegher/sprapp/SearchActivity.java
@@ -14,20 +14,21 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 import it.unive.dais.cevid.datadroid.lib.parser.EntiParser;
+import it.unive.dais.cevid.datadroid.lib.parser.SoldipubbliciParser;
+import it.unive.dais.cevid.datadroid.lib.util.DataManipulation;
 import it.unive.dais.cevid.datadroid.lib.util.Function;
 
 public class SearchActivity extends AppCompatActivity {
 
     private static final String TAG = "SearchActivity";
 
-    private LinearLayout mainView;
     private EntiParser<?> entiParser;
-    private String searchText = "";
     private List entiList;
     private ListView lv;
     ArrayAdapter<String> adapter;
@@ -41,21 +42,18 @@ public class SearchActivity extends AppCompatActivity {
 
         lv = (ListView) findViewById(R.id.list_view);
         inputSearch = (EditText) findViewById(R.id.inputSearch);
-
         entiList = new ArrayList<EntiParser.Data>();
-
         entiParser = new EntiParser();
-        entiParser.getAsyncTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
 
+        entiParser.getAsyncTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
         nomeEnti = new ArrayList<>();
+
 
         try {
             List<EntiParser.Data> l = new ArrayList<>(entiParser.getAsyncTask().get());
             for (EntiParser.Data x : l) {
-                if (x.codice_comparto.equals("PRO") || x.codice_comparto.equals("REG")) {
-                    //Log.d("PROVA", x.descrizione_ente);
+                if (x.codice_comparto.equals("PRO") || x.codice_comparto.equals("REG"))
                     entiList.add(x);
-                }
             }
         } catch (InterruptedException | ExecutionException e) {
             e.printStackTrace();
@@ -65,6 +63,7 @@ public class SearchActivity extends AppCompatActivity {
             nomeEnti.add(((EntiParser.Data) x).descrizione_ente);
 
         adapter = new ArrayAdapter<String>(this, R.layout.list_enti, R.id.DescrizioneEnte, nomeEnti);
+
         lv.setAdapter(adapter);
 
         inputSearch.addTextChangedListener(new TextWatcher() {
@@ -87,107 +86,61 @@ public class SearchActivity extends AppCompatActivity {
         lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent i = new Intent(SearchActivity.this, SearchableActivity.class);
-            }
-        });
+                String numero_abitanti, codice_comparto, codice_ente, descrizione_ente = adapter.getItem(position);
+                List speseEnte = new ArrayList<SoldipubbliciParser.Data>();
 
-        /*setSingleListener(soldiPubblicireloadedSearch, entiParser, SearchableActivity.LIST_ENTI_TERRITORIALI, Enti_getText, Enti_getCode, new Function<String, Void>() {
-            @Override
-            public Void eval(String x) {
-                SearchActivity.this.searchText = x;
-                return null;
-            }
-        });
+                codice_comparto = findCodiceCompartoByDescrizioneEnte(descrizione_ente);
+                codice_ente = findCodiceEnteByDescrizioneEnte(descrizione_ente);
+                numero_abitanti = findNumeroAbitantiByDescrizioneEnte(descrizione_ente);
 
-        //Button combineButton = (Button) findViewById(R.id.button_data);
-        /*combineButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(SearchActivity.this, com.example.gianmarcocallegher.sprapp.SearchableActivity.class);
-                boolean r1 = processQuery(entiParser, searchText, intent, com.example.gianmarcocallegher.sprapp.SearchableActivity.LIST_COMUNI, Enti_getText, Enti_getCode);
-                if (r1)
-                    startActivity(intent);
-                else
-                    alert("Compilare il campo di ricerca");
-            }
-        });
+                SoldipubbliciParser soldipubbliciParser = new SoldipubbliciParser(codice_comparto, codice_ente);
 
-        mainView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
-                    hideKeyboard(v);
+                soldipubbliciParser.getAsyncTask().executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+
+                try {
+                    speseEnte = new ArrayList<>(soldipubbliciParser.getAsyncTask().get());
+                } catch (InterruptedException | ExecutionException e) {
+                    e.printStackTrace();
                 }
-            }
-        });*/
-    }
 
-    /*private <T> void setSingleListener(final SearchView v, final AsyncParser<T, ?> parser, final String label,
-                                       final Function<T, String> getText, final Function<T, Integer> getCode, final Function<String, Void> setText) {
-        v.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String text) {
                 Intent intent = new Intent(SearchActivity.this, SearchableActivity.class);
-                if (processQuery(parser, text, intent, label, getText, getCode))
-                    startActivity(intent);
-                else
-                    alert("La ricerca non ha prodotto nessun risultato. Provare con altri valori.");
-                v.clearFocus();
-                return true;
-            }
 
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                setText.eval(newText);
-                return false;
+                intent.putExtra("numero_abiatanti", numero_abitanti);
+                intent.putExtra("descrizione_ente", descrizione_ente);
+                intent.putExtra("spese_ente", (Serializable) speseEnte);
+
+                startActivity(intent);
             }
         });
     }
 
-    private <T> boolean processQuery(AsyncParser<T, ?> parser, String text, Intent intent, String label,
-                                     Function<T, String> getText, Function<T, Integer> getCode) {
-        try {
-            List<T> l = new ArrayList<>(parser.getAsyncTask().get());
-            if (!text.isEmpty()) {
-                if (text.matches("[0-9]+"))
-                    DataManipulation.filterByCode(l, Integer.parseInt(text), getCode);
-                else
-                    DataManipulation.filterByWords(l, text.split(" "), getText, false);
-                if (l.size() == 0) {
-                    return false;
-                } else {
-                    intent.putExtra(label, (Serializable) l);
-                    return true;
-                }
+    public String findCodiceCompartoByDescrizioneEnte(String descrizione_ente) {
+        String codice_comparto = new String();
+        for (Object x : entiList) {
+            if (((EntiParser.Data) x).descrizione_ente.equals(descrizione_ente)) {
+                codice_comparto = new String(((EntiParser.Data) x).codice_comparto);
             }
-        } catch (InterruptedException | ExecutionException e) {
-            alert(String.format("Errore inatteso: %s. Riprovare.", e.getMessage()));
-            e.printStackTrace();
         }
-        return false;
+        return codice_comparto;
     }
 
-    private void alert(String msg) {
-        Snackbar.make(mainView, msg, Snackbar.LENGTH_SHORT).show();
+    public String findCodiceEnteByDescrizioneEnte(String descrizione_ente) {
+        String codice_ente = new String();
+        for (Object x : entiList) {
+            if (((EntiParser.Data) x).descrizione_ente.equals(descrizione_ente)) {
+                codice_ente = new String(((EntiParser.Data) x).codice_ente);
+            }
+        }
+        return codice_ente;
     }
 
-    private void hideKeyboard(View v) {
-        InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(Activity.INPUT_METHOD_SERVICE);
-        inputMethodManager.hideSoftInputFromWindow(v.getWindowToken(), InputMethodManager.HIDE_IMPLICIT_ONLY);
-    }*/
-
-    private static final Function<EntiParser.Data, String> Enti_getText = new Function<EntiParser.Data, String>() {
-        @Override
-        public String eval(EntiParser.Data x) {
-            return x.codice_sottocomparto;
+    public String findNumeroAbitantiByDescrizioneEnte(String descrizione_ente) {
+        String numero_abitanti = new String();
+        for (Object x : entiList) {
+            if (((EntiParser.Data) x).descrizione_ente.equals(descrizione_ente)) {
+                numero_abitanti = new String(((EntiParser.Data) x).numero_abitanti);
+            }
         }
-    };
-
-    private static final Function<EntiParser.Data, Integer> Enti_getCode = new Function<EntiParser.Data, Integer>() {
-        @Override
-        public Integer eval(EntiParser.Data x) {
-            return Integer.parseInt(x.codice_comune);
-        }
-    };
-
+        return numero_abitanti;
+    }
 }
--- samples/SPRApp/app/src/main/java/com/example/gianmarcocallegher/sprapp/SearchableActivity.java
+++ samples/SPRApp/app/src/main/java/com/example/gianmarcocallegher/sprapp/SearchableActivity.java
@@ -1,9 +1,17 @@
 package com.example.gianmarcocallegher.sprapp;
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-public class SearchableActivity extends ListActivity {
+import java.io.Serializable;
+import java.util.List;
+
+import it.unive.dais.cevid.datadroid.lib.parser.SoldipubbliciParser;
+
+public class SearchableActivity extends AppCompatActivity {
 
     public static final String LIST_COMUNI = "LIST_ENTI_TERRITORIALI";
 
@@ -11,5 +19,12 @@ public class SearchableActivity extends ListActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_searchable);
+
+        Intent i = getIntent();
+        String numero_abitanti = i.getStringExtra("numero_abitanti");
+        String descrizione_ente = i.getStringExtra("descrizione_ente");
+        Serializable sl = i.getSerializableExtra("spese_ente");
+        List<SoldipubbliciParser.Data> spese_ente = (List<SoldipubbliciParser.Data>) sl;
+
     }
 }
