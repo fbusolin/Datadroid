4346157cf3d3ab4cdecc53d68e27309635216237
Unknown <851884@stud.unive.it>
2017-11-24 13:57:17 +0100

    fix mancanza Toolbar

--- samples/AAC/app/src/main/java/it/unive/dais/cevid/aac/MainActivity.java
+++ samples/AAC/app/src/main/java/it/unive/dais/cevid/aac/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
-import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
